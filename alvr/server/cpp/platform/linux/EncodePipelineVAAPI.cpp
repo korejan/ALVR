0d7adf2e7be6720c0a67fbe68eee6a5c24d93bff
Yury <RIP95_95@mail.ru>
2023-01-04 22:31:59 +0200

    Add VBAQ option (#1336)
    * Add VBAQ option to AMF
    
    * Add VBAQ to VAAPI

--- alvr/server/cpp/platform/linux/EncodePipelineVAAPI.cpp
+++ alvr/server/cpp/platform/linux/EncodePipelineVAAPI.cpp
@@ -161,7 +161,7 @@ alvr::EncodePipelineVAAPI::EncodePipelineVAAPI(std::vector<VkFrame>& input_frame
   
   vlVaQualityBits quality = {};
   quality.valid_setting = 1;
-  quality.vbaq_mode = 1;  //No noticable performance difference and should improve subjective quality by allocating more bits to smooth areas
+  quality.vbaq_mode = Settings::Instance().m_enableVbaq;  //No noticable performance difference and should improve subjective quality by allocating more bits to smooth areas
   switch (settings.m_encoderQualityPreset)
   {
     case ALVR_QUALITY:
