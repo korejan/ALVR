4c5dd806f8f18d93a42f3dc709241a4a3db70a2f
korejan <64199710+korejan@users.noreply.github.com>
2023-07-03 21:29:55 +0100

    bump default ffmpeg version for linux builds.

--- alvr/xtask/src/main.rs
+++ alvr/xtask/src/main.rs
@@ -765,7 +765,7 @@ fn main() {
         let version: Option<String> = args.opt_value_from_str("--version").unwrap();
         let root: Option<String> = args.opt_value_from_str("--root").unwrap();
 
-        let default_var = String::from("release/6.0");
+        let default_var = String::from("release/5.1");
         let mut ffmpeg_version: String =
             args.opt_value_from_str("--ffmpeg-version").unwrap().map_or(
                 default_var.clone(),
