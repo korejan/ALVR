dd64b12967afca0afde43f7ec9b22228381dccc0
korejan <64199710+korejan@users.noreply.github.com>
2023-08-01 21:37:05 +0100

    expose hand-tracking for library usage.

--- alvr/openxr-client/alxr-engine-sys/src/lib.rs
+++ alvr/openxr-client/alxr-engine-sys/src/lib.rs
@@ -89,6 +89,7 @@ impl ALXRSystemProperties {
             refreshRatesCount: 0,
             recommendedEyeWidth: 0,
             recommendedEyeHeight: 0,
+            enabledTrackingSystemsFlags: 0,
         }
     }
 
