453df87ecb797401aa392be932b966f344f3bd25
korejan <64199710+korejan@users.noreply.github.com>
2025-03-09 22:47:39 +0000

    Adds support for color quantization scheme option

--- alvr/session/src/lib.rs
+++ alvr/session/src/lib.rs
@@ -60,6 +60,7 @@ pub struct OpenvrConfig {
     pub aggressive_keyframe_resend: bool,
     pub adapter_index: u32,
     pub codec: u32,
+    pub color_range: u32,
     pub refresh_rate: u32,
     pub use_10bit_encoder: bool,
     pub enable_vbaq: bool,
