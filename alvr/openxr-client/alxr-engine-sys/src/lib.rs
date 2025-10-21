e223a45308047441326396673af868c735eb598e
korejan <64199710+korejan@users.noreply.github.com>
2025-10-21 23:17:19 +0100

    replaces XR_ANDROID_avatar_eyes with XR_ANDROID_eye_tracking

--- alvr/openxr-client/alxr-engine-sys/src/lib.rs
+++ alvr/openxr-client/alxr-engine-sys/src/lib.rs
@@ -75,7 +75,7 @@ impl From<&str> for crate::ALXREyeTrackingType {
             "None" => crate::ALXREyeTrackingType::None,
             "FBEyeTrackingSocial" => crate::ALXREyeTrackingType::FBEyeTrackingSocial,
             "ExtEyeGazeInteraction" => crate::ALXREyeTrackingType::ExtEyeGazeInteraction,
-            "AndroidAvatarEyes" => crate::ALXREyeTrackingType::AndroidAvatarEyes,
+            "AndroidEyeTracking" => crate::ALXREyeTrackingType::AndroidEyeTracking,
             _ => crate::ALXREyeTrackingType::Auto,
         }
     }
